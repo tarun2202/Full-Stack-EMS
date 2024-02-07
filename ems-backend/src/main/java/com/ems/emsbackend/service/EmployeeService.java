b0c8a17c023247e45c332535597a13edf80fc78f
tarun.2202 <kumartk.2202@gmail.com>
2024-02-08 00:36:47 +0530

    added EmployeeService interface

---
+++ ems-backend/src/main/java/com/ems/emsbackend/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.ems.emsbackend.service;
+
+
+
+import com.ems.emsbackend.dto.EmployeeDto;
+
+
+public interface EmployeeService {
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+}
