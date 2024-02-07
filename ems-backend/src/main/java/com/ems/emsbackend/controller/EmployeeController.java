1ffe3592d2b4dc3049c6bb0a3a0677bbb4056519
tarun.2202 <kumartk.2202@gmail.com>
2024-02-08 00:44:08 +0530

    added EmployeeController

---
+++ ems-backend/src/main/java/com/ems/emsbackend/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.ems.emsbackend.controller;
+
+import com.ems.emsbackend.dto.EmployeeDto;
+import com.ems.emsbackend.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin("*")
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    // Build Add Employee REST API
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
+        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+
+}
