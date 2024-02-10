328ca3c7c732da7eca9a50f362ff8157f73bddc2
tarun.2202 <kumartk.2202@gmail.com>
2024-02-10 23:49:54 +0530

    Route added for ListAllEmployee

--- emd-frontend/src/App.jsx
+++ emd-frontend/src/App.jsx
@@ -2,14 +2,27 @@ import './App.css'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeeComponent from './components/ListEmployeeeComponent'
+import { BrowserRouter, Routes , Route} from 'react-router-dom'
+
 
 function App() {
 
   return (
     <>
+    <BrowserRouter>
       <HeaderComponent/>
-      <ListEmployeeeComponent/>
+      <Routes>
+        
+          {/* //http://localhost:3000 */}
+          <Route path = '/' element = {<ListEmployeeeComponent />}></Route>
+
+          {/* //http:localhost:8080/employees */}
+          <Route path = '/employees' element = {<ListEmployeeeComponent />}></Route>
+        
+      </Routes>
       <FooterComponent/>
+    </BrowserRouter>
+      
     </>
   )
 }
