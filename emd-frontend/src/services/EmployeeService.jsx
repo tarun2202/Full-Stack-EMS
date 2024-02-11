58d66dc9cdca64609fd8c964856cb56d159ac26c
tarun.2202 <kumartk.2202@gmail.com>
2024-02-11 23:20:56 +0530

    Create Employee service added

--- emd-frontend/src/services/EmployeeService.jsx
+++ emd-frontend/src/services/EmployeeService.jsx
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const REST_API_BASE_URL = 'http://localhost:8080/api/employees';
 
-const listEmployees = () => {
-    return axios.get(REST_API_BASE_URL);
-}
+export const listEmployees = () => axios.get(REST_API_BASE_URL);
 
-export default listEmployees;
+export const createEmployee = (employee) => axios.post(REST_API_BASE_URL, employee);
+
